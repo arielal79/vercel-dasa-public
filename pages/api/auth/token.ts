6a72d89e1b0c80f9f91a384f50bee90fdce91fce
arielal79 <alariel739@gmail.com>
2024-11-17 19:39:26 -0300

    varios requisitos

--- pages/api/auth/token.ts
+++ pages/api/auth/token.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import methods from "micro-method-router";
 
 export default methods({
-  async get(req: NextApiRequest, res: NextApiResponse) {
-    res.send("soy el me");
+  async post(req: NextApiRequest, res: NextApiResponse) {
+    res.send("soy el token");
   },
 });
--- pages/api/products/[productId].ts
+++ pages/api/products/[productId].ts
@@ -4,6 +4,6 @@ import methods from "micro-method-router";
 
 export default methods({
   async get(req: NextApiRequest, res: NextApiResponse) {
-    res.send("soy el me");
+    res.send("soy el get, productId " + req.query.productId);
   },
 });
